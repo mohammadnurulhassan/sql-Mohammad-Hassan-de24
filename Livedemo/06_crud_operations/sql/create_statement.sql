18fca33f48698e8d30123390ff1d50f38fbd36de
mohammadnurulhassan <mdnrlhsn@gmail.com>
2024-12-11 10:13:28 +0100

    update to new

--- Livedemo/06_crud_operations/sql/create_statement.sql
+++ Livedemo/06_crud_operations/sql/create_statement.sql
@@ -6,7 +6,11 @@ SELECT * FROM information_schema.schemata;
 
 CREATE SEQUENCE IF NOT EXISTS id_duckdb_sequence START 1;
 
-SELECT * FROM pg_catalog.pg_sequences;
+SELECT
+	*
+FROM
+	pg_catalog.pg_sequences;
+
 CREATE TABLE IF NOT EXISTS database.duckdb (
 id INTEGER DEFAULT nextval('id_duckdb_sequence'),
 word STRING,
@@ -14,3 +18,16 @@ description STRING
 );
 
 
+
+CREATE TABLE IF NOT EXISTS database.sql (
+	word STRING,
+	description STRING);
+
+CREATE SCHEMA IF NOT EXISTS programming;
+CREATE TABLE IF NOT EXISTS programming.python (
+	word STRING,
+	description STRING);
+
+CREATE TABLE IF NOT EXISTS programming.c_sharp (
+	word STRING,
+	description STRING);
